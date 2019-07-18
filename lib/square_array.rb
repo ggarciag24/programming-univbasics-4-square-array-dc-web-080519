9e3190a3d99f49dc911cdc41537c159b287a92ab
German Garcia <ggarciag.business@outlook.com>
2019-07-18 01:06:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,9 +4,9 @@ def square_array(arr)
     counter = 0
       while counter <= arr.length - 1 do
         # binding.pry
-        newAr = arr[counter] = arr[counter] * arr[counter]
+        arr[counter] = arr[counter] * arr[counter]
         counter += 1
       end
-      binding.pry
-      return newAr
+      # binding.pry
+      return arr
 end
