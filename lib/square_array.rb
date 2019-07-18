34b50ee7b0106edfce60b60f26b696cc9530c31c
German Garcia <ggarciag.business@outlook.com>
2019-07-18 00:49:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,11 @@
+require('pry')
+
 def square_array(array)
     counter = 0
       while counter <= array.length do
         newAr = array[counter] *= array[counter]
         counter += 1
       end
+      binding.pry
       return newAr
 end
